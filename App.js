6c84ac2f16addcda6298d9eaf16120f7e2100ef2
6derick6 <6derick6@gmail.com>
2023-03-15 22:56:40 -0300

    app MyNotes 3/4

--- App.js
+++ App.js
@@ -5,26 +5,48 @@ import { StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-nativ
 export default function App() {
 
   const [estado,setEstado] = useState('leitura');
-  const [anotacao,setAnotacao] = useState('Maecenas in felis sit amet elit volutpat dictum. Curabitur a diam magna. In vitae massa leo. Phasellus blandit, turpis sit amet tempus venenatis, nulla tortor faucibus nisi, et vestibulum mi velit vel eros. Duis ultrices rutrum cursus. Etiam scelerisque lectus a lobortis aliquet. Integer semper est non iaculis aliquet. Phasellus elementum egestas posuere.');
+  const [anotacao,setAnotacao] = useState('');
+
+  function atualizarTexto(){
+    setEstado('leitura');
+  }
 
   if(estado == 'leitura'){
   return (
-    <View style={{flex:1}}>
+    <View style={{flex:1,marginTop:30}}>
       <View style={styles.header}><Text style={{textAlign:'center',color:'white',fontSize:18}}>App MyNotes</Text></View>
 
+      {
+      (anotacao != '')?  
       <View style={{padding:20}}><Text style={styles.anotacao}>{anotacao}</Text></View>
+      :
+      <View style={{padding:20}}><Text style={{opacity:0.3}}>Nenhuma anotação encontrada :(</Text></View>
+      }
 
-      <TouchableOpacity onPress={()=> setEstado('atualizando')} style={styles.btnAnotacao}><Text style={styles.btnAnotacaoTexto}>+</Text></TouchableOpacity>  
+      
+      
+      {
+      (anotacao == '')?  
+      <TouchableOpacity onPress={()=> setEstado('atualizando')} style={styles.btnAnotacao}>
+        <Text style={styles.btnAnotacaoTexto}>+</Text>
+      </TouchableOpacity>   
+      :
+      <TouchableOpacity onPress={()=> setEstado('atualizando')} style={styles.btnEditar}>
+        <Text style={styles.btnEditarTexto}>Editar</Text>
+      </TouchableOpacity>  
+      }
+
+       
     </View>
   );
   }else if(estado == 'atualizando'){
     return (
-      <View style={{flex:1}}>
+      <View style={{flex:1,marginTop:30}}>
         <View style={styles.header}><Text style={{textAlign:'center',color:'white',fontSize:18}}>App MyNotes</Text></View>
   
         <TextInput style={{padding:20,height:300,textAlignVertical:'top'}} onChangeText={(text)=>setAnotacao(text)} multiline={true} numberOfLines={5} value={anotacao}></TextInput>
 
-        <TouchableOpacity onPress={()=> setEstado('leitura')} style={styles.btnSalvar}><Text style={styles.btnSalvarTexto}>Salvar</Text></TouchableOpacity>  
+        <TouchableOpacity onPress={()=> atualizarTexto()} style={styles.btnSalvar}><Text style={styles.btnSalvarTexto}>Salvar</Text></TouchableOpacity>  
       </View>
     );
   }
@@ -77,6 +99,24 @@ const styles = StyleSheet.create({
     top: 0,
     fontSize:15
   },
+
+  btnEditar:{
+    position:'absolute',
+    right:0,
+    bottom:20,
+    width:'100%',
+    paddingTop: 10,
+    paddingBottom:10,
+    backgroundColor:'#069'
+  },
+
+  btnEditarTexto:{
+    color: 'white',
+    position: 'relative',
+    textAlign:'center',
+    top: 0,
+    fontSize:15
+  }
 
 });
 
